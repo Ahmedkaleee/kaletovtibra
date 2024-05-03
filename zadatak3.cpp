fe6f2d8c70749d2855e3fe10fe2c32427bc1c9de
Ahmedkaleee <168560106+Ahmedkaleee@users.noreply.github.com>
2024-05-03 15:27:44 +0200

    Create zadatak3.cpp

---
+++ zadatak3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Definicija funkcije pozdrav sa parametrom godine
+void pozdrav(int godine) {
+    cout << "Ime: [Vaše ime]" << endl;
+    cout << "Prezime: [Vaše prezime]" << endl;
+    cout << "Godine: " << godine << endl;
+}
+
+int main() {
+    int mojeGodine = 25; // Možete promijeniti ovo na svoje godine
+
+    // Poziv funkcije pozdrav sa godinama kao argumentom
+    pozdrav(mojeGodine);
+    
+    return 0;
+}
