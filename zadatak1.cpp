72824ad24e6d00d9bd6891055166bef17d9915df
Ahmedkaleee <168560106+Ahmedkaleee@users.noreply.github.com>
2024-05-03 15:25:00 +0200

    Create zadatak1.cpp

---
+++ zadatak1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Definicija funkcije pozdrav
+void pozdrav() {
+    cout << "Ime: [Ahmed]" << endl;
+    cout << "Prezime: [Katica]" << endl;
+    cout << "Škola: [Perzisko bosanski koledz]" << endl;
+}
+
+int main() {
+    // Poziv funkcije pozdrav
+    pozdrav();
+    
+    return 0;
+}
